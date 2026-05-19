b5be0d3da9a7391ad7c6480e4974220c2e3242b8
Marina <sdi2300302@di.uoa.gr>
2026-05-19 17:38:36 +0300

    auth

---
+++ frontend/src/components/Authorisation.tsx
@@ -0,0 +1,28 @@
+
+// type Role = "admin" | "organiser" | "user" | "guest";
+ 
+// //  Creating a user type
+// interface User {
+//   id: string;
+//   name: string;
+//   email: string;
+//   role: Role;          // Their assigned role
+//   isActive: boolean;   //  Account status
+// }
+
+// //  Authorization decorator
+// function requireRole(allowedRoles: Role[]) {
+//   return function (target: any, propertyName: string, descriptor: PropertyDescriptor) {
+//     const method = descriptor.value;
+    
+//     descriptor.value = function (...args: any[]) {
+//       const user: User = this.currentUser; //  Get current user
+      
+//       if (!allowedRoles.includes(user.role)) {
+//         throw new Error(` Access denied. Required roles: ${allowedRoles.join(', ')}`);
+//       }
+      
+//       return method.apply(this, args);
+//     };
+//   };
+// }
