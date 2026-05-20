d6a18daa135cdc04014c52edaad61fe465ad7410
Marina <sdi2300302@di.uoa.gr>
2026-05-20 10:41:57 +0300

    login fetching ready

--- frontend/src/components/Authorisation.tsx
+++ frontend/src/components/Authorisation.tsx
@@ -1,28 +1,33 @@
 
-// type Role = "admin" | "organiser" | "user" | "guest";
+ type Role = "admin" | "organiser" | "user" | "guest";
  
-// //  Creating a user type
-// interface User {
-//   id: string;
-//   name: string;
-//   email: string;
-//   role: Role;          // Their assigned role
-//   isActive: boolean;   //  Account status
-// }
+   //Creating a user type
+ interface User {
+   id: string;
+   firstName: string;
+   lastName: string;
+   email:string
+   password: string;
+   phoneNumber: string;
+   address: string;
+   TIN: string;
+   role: Role;           //Their assigned role
+   isActive: boolean;     //Account status
+ }
 
-// //  Authorization decorator
-// function requireRole(allowedRoles: Role[]) {
-//   return function (target: any, propertyName: string, descriptor: PropertyDescriptor) {
-//     const method = descriptor.value;
+   //Authorization decorator
+ function requireRole(allowedRoles: Role[]) {
+   return function (target: any, propertyName: string, descriptor: PropertyDescriptor) {
+     const method = descriptor.value;
     
-//     descriptor.value = function (...args: any[]) {
-//       const user: User = this.currentUser; //  Get current user
+     descriptor.value = function (...args: any[]) {
+       const user: User = this.currentUser;   //Get current user
       
-//       if (!allowedRoles.includes(user.role)) {
-//         throw new Error(` Access denied. Required roles: ${allowedRoles.join(', ')}`);
-//       }
+       if (!allowedRoles.includes(user.role)) {
+         throw new Error(` Access denied. Required roles: ${allowedRoles.join(', ')}`);
+       }
       
-//       return method.apply(this, args);
-//     };
-//   };
-// }
+       return method.apply(this, args);
+     };
+   };
+ }
