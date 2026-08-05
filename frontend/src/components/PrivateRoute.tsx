8fb3c489b5dfdf7518b09daa3a40baf103ec6971
Marina <sdi2300302@di.uoa.gr>
2026-08-05 23:21:21 +0300

    frontend authorization

--- frontend/src/components/PrivateRoute.tsx
+++ frontend/src/components/PrivateRoute.tsx
@@ -1,5 +1,6 @@
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { useAuth } from '../Auth/useAuth.ts';
+import type { Role } from '../Auth/Authentication.tsx';
 
 export function PrivateRoute() {
     const { user, loading } = useAuth();
@@ -9,13 +10,40 @@ export function PrivateRoute() {
         return <p aria-live="polite">Checking session...</p>;
     }
 
-    return user ? (
-        <Outlet />
-    ) : (
-        <Navigate
+    if (!user) {
+        return <Navigate
             to="/login"
             replace
-            state={{ from: location }} // remember original page
         />
-    );
+    }
+
+    // Check user roles 
+    const userRole = user?.role;
+    const currentPage = Object.keys(pagePermissions).find(path => location.pathname.startsWith(path))
+    const isAuthorized = currentPage ? pagePermissions[currentPage].includes(userRole) : false // fallback to false
+
+    return isAuthorized
+        ? <Outlet />
+        : <Navigate
+            to="/home"
+            replace
+        />
+
+
 }
+
+export const pagePermissions: Record<string, Role[]> = {
+    "/home":                ["ADMIN", "ORGANIZER", "USER"],
+    "/welcome":             ["ADMIN", "ORGANIZER", "USER"],
+    "/login":               ["ADMIN", "ORGANIZER", "USER"],
+    "/register":            ["ADMIN", "ORGANIZER", "USER"],
+    "/users":               ["ADMIN"],
+    "/messaging":           ["ADMIN", "ORGANIZER", "USER"],
+    "/events":              ["ADMIN", "ORGANIZER", "USER"],
+    "/createEvent":         ["ADMIN", "ORGANIZER"],
+    "/editEvent":           ["ADMIN", "ORGANIZER"],
+    "/profile":             ["ADMIN", "ORGANIZER", "USER"],
+    "/booking":             ["ADMIN", "ORGANIZER", "USER"],
+    "/myBookings":          ["ADMIN", "ORGANIZER", "USER"],
+
+};
