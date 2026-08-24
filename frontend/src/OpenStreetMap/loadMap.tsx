b5cd155c6c626ab90caf818fcfff5f10e2a7bec4
Marina <sdi2300302@di.uoa.gr>
2026-08-24 21:11:49 +0300

    fix map marker

--- frontend/src/OpenStreetMap/loadMap.tsx
+++ frontend/src/OpenStreetMap/loadMap.tsx
@@ -1,6 +1,10 @@
 import { useEffect } from "react";
-import { map, tileLayer, marker } from "leaflet";
+import { map, tileLayer, marker, Icon } from "leaflet";
 import "leaflet/dist/leaflet.css";
+
+import markerIconUrl from "../../node_modules/leaflet/dist/images/marker-icon.png";
+import markerIconRetinaUrl from "../../node_modules/leaflet/dist/images/marker-icon-2x.png";
+import markerShadowUrl from "../../node_modules/leaflet/dist/images/marker-shadow.png";
 
 
 interface EventMapProps {
@@ -15,6 +19,12 @@ export default function EventMap({
   longitude,
   title
 }: EventMapProps) {
+
+  // fix for leaflet marker
+  Icon.Default.prototype.options.iconUrl = markerIconUrl;
+  Icon.Default.prototype.options.iconRetinaUrl = markerIconRetinaUrl;
+  Icon.Default.prototype.options.shadowUrl = markerShadowUrl;
+  Icon.Default.imagePath = ""; // necessary to avoid Leaflet adds some prefix to image path.
 
 
   useEffect(() => {
