8e2c5d7c1a5ca70b97170df5e3003446c8e35fef
asomkyriakos <asomkyriakos@gmail.com>
2026-08-31 15:52:59 +0300

    First steps for messsagesx

---
+++ backend/src/main/java/com/eventPlatform/backend/repository/MessagesRepository.java
@@ -0,0 +1,11 @@
+package com.eventPlatform.backend.repository;
+
+import com.eventPlatform.backend.entity.Messages;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MessagesRepository  extends JpaRepository<Messages, Long> {
+    List<Messages> findByRecipientId(Long RecipientId);
+    List<Messages> findBySenderId(Long SenderId);
+}
