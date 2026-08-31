591f00340e8ba906737b5e3fe3d0403f12421fdc
asomkyriakos <asomkyriakos@gmail.com>
2026-08-31 15:52:59 +0300

    First steps for messsagesx

---
+++ backend/src/main/java/com/eventPlatform/backend/service/MessagesService.java
@@ -0,0 +1,10 @@
+package com.eventPlatform.backend.service;
+
+import com.eventPlatform.backend.entity.Messages;
+import com.eventPlatform.backend.repository.MessagesRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public class MessagesService {
+}
