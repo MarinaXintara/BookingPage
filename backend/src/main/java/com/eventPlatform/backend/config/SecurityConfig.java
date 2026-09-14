8a75e60bd53c7aa8fa9addb75748e0ee9d45e7f4
asomkyriakos <asomkyriakos@gmail.com>
2026-09-14 11:38:52 +0300

    fixed

--- backend/src/main/java/com/eventPlatform/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/eventPlatform/backend/config/SecurityConfig.java
@@ -30,12 +30,8 @@ public class SecurityConfig {
                 .requestMatchers("/api/events/**").permitAll()
                 .requestMatchers("/api/Booking/**").permitAll()
                 .requestMatchers("/api/messages/**").permitAll()
-<<<<<<< HEAD
                 .requestMatchers("/api/users/**").permitAll() //use later hasRole("ADMIN")
-=======
-                .requestMatchers("/api/users/**").permitAll()
-                                .requestMatchers("/api/recommendation/**").permitAll() //use later hasRole("ADMIN")
->>>>>>> 4014c0985517a58b3b9df72ed555c05c0df32893
+    .            requestMatchers("/api/recommendation/**").permitAll() //use later hasRole("ADMIN")
                 .anyRequest().authenticated()
                 )
                 .addFilterBefore(
